9aceb1b348f0e15a1a2f5d752e038cf40d2f5aa9
SebastianTriana <setrianag@unal.edu.co>
2025-09-09 20:45:51 -0500

    Update clases.py
    Cambie el codigo para que ahora hagamos feliz a un perro dandole comida, juguetes y dandole caricias

--- clases.py
+++ clases.py
@@ -1,8 +1,11 @@
 #Autor: Sebastián Triana
 
+
 #Estos son ejercicios con clases
 
-#Perros y Gatos
+
+#Caso de uso: Hacer al perro feliz(Darle comida juguetes y acariciarlo)
+
 
 class Perro:
     def __init__(self, raza, color):
@@ -10,26 +13,48 @@ class Perro:
         self.color = color
 
 
-    def ladrar(self):
-        print("GUAAAAAAAUU GUAAAUUU!!!!")
+    def dar_juguete(self, juguete):
+        if juguete == "S":
+            return True
+        if juguete == "N":
+            return False
+        return False
 
-class Gato:
-    def __init__(self, raza, color):
-        self.raza = raza
-        self.color = color
 
-    def maullar(self):
-        print("MIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUUUUUUUUUUUUUUUU!!!!")
+    def dar_comida(self, comida):
+        if comida == "S":
+            return True
+        if comida == "N":
+            return False
+        return False
+
+
+    def dar_acariciar(self, caricia):
+        if caricia == "S":
+            return True
+        if caricia == "N":
+            return False
+        return False
+
+
+
 
 def main():
     raza_perro = input("Ingrese la raza de su perro: ")
     color_perro = input("Ingrese el color de su perro: ")
-    mi_perro = Perro(raza_perro, color_perro)
-    mi_perro.ladrar()
+    juguete = input("Dar juguete? S/N  ").upper()
+    comida = input("Dar comida? S/N  ").upper()
+    caricia = input("Acariciar? S/N  ").upper()
 
-    raza_gato = input("Ingrese la raza de su gato: ")
-    color_gato = input("Ingrese el color de su gato: ")
-    mi_gato = Gato(raza_gato, color_gato)
-    mi_gato.maullar()
+
+    mi_perro = Perro(raza_perro, color_perro)
+
+
+    if mi_perro.dar_juguete(juguete) and mi_perro.dar_comida(comida) and mi_perro.dar_acariciar(caricia):
+        print(f"Tu {raza_perro} de color {color_perro} está feliz!! GUAAAUU GUAUU")
+    else:
+        print(f"Tu {raza_perro} de color {color_perro} está más triste que 10 tristes.")
+
 
 main()
+
